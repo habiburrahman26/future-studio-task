5638cdc9c177fe3448c9ff6e7ed5565bc7c0b798
habiburrahman26 <habib264521@gmail.com>
2026-09-19 18:56:31 +0600

    fix: update BASE_URL to use NEXT_PUBLIC_BASE_URL for correct API access

--- features/product/services.ts
+++ features/product/services.ts
@@ -1,7 +1,7 @@
 import { Product, ProductFilters, ProductsResponse } from './types';
 import productsData from '@/data/products.json';
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 
 /**
